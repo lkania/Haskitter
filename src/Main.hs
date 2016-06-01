b752dab4d4e711ed11b2549ba51796812323f0cf
lkania <lkania@itba.edu.ar>
2016-06-01 01:46:13 -0300

    add API REST access to user profile

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,19 @@ module Main(Haskitter,main) where
 import Snap
 import Control.Lens
 import Snap.Snaplet
-import Snap.Snaplet (Handler)
+
 import Snap.Snaplet.PostgresqlSimple
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.List
 import Control.Applicative
-import Database.PostgreSQL.Simple.FromRow
 import Snap.Extras
 import Data.Aeson
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
 import GHC.Generics
+import Control.Monad.State.Class
+import qualified Data.ByteString as BS
 
 -- | The Memoise type identifies our application and holds anything our snaplet needs to function.
 data Haskitter = Haskitter
@@ -24,7 +27,7 @@ makeLenses ''Haskitter
 hashkitterInit :: SnapletInit Haskitter Haskitter
 hashkitterInit = makeSnaplet "hashkitterInit" "Haskell twitter, 'cause YOLO" Nothing $ do
   p <- nestSnaplet "pg" pg pgsInit
-  addRoutes [("/posts", postsIndexHandler),("/users", usersIndexHandler)]
+  addRoutes [("/posts", postsIndexHandler),("/users", usersIndexHandler),("/user/:id",userHandler)]
   return $ Haskitter { _pg = p }
 
 main :: IO ()
@@ -36,10 +39,10 @@ main = do
 
 data Post = Post { message	::	String } 
 
-data User = User { email :: String, name :: String, password_digest :: String }
+data User = User { uid :: Int, email :: String, name :: String, password_digest :: String }
 
 instance FromRow User where
-  fromRow = User <$> field <*> field <*> field
+  fromRow = User <$> field <*> field <*> field <*> field
 
 instance FromRow Post where
   fromRow = Post <$> field
@@ -49,17 +52,41 @@ instance ToJSON Post where
     object ["message" Data.Aeson..= message]
 
 instance ToJSON User where
-  toJSON(User email name password_digest) =
-    object ["name" Data.Aeson..= name]
+  toJSON(User uid email name password_digest) =
+    object ["name" Data.Aeson..= name, "id" Data.Aeson..= uid]
 
+instance HasPostgres (Handler b Haskitter) where
+	getPostgresState = with pg get
 
 postsIndexHandler :: Handler Haskitter Haskitter ()
 postsIndexHandler = do
-  allPosts <- with pg $ query_ "SELECT message FROM posts"
+  allPosts <- query_ "SELECT message FROM posts"
   writeJSON (allPosts :: [Post])
 
 -- name is a reserved keyword in postgreSQL, hence it must be escaped in order to do a quer
 usersIndexHandler :: Handler Haskitter Haskitter ()
 usersIndexHandler = do
-  allUsers <- with pg $ query_ "SELECT email,\"name\",password_digest FROM users"
-  writeJSON (allUsers :: [User])
+	modifyResponse $ setHeader "Content-Type" "application/json"
+	users <- getUsers
+	writeLBS . encode $ (users :: [User])
+
+userHandler :: Handler Haskitter Haskitter ()
+userHandler = do
+	modifyResponse $ setHeader "Content-Type" "application/json"
+	user_id <- getParam "id"
+	maybe (writeBS "User does not exist") userHandler' user_id
+
+userHandler' :: BS.ByteString -> Handler Haskitter Haskitter ()
+userHandler' user_id = do
+	user <- (getUser $ (T.unpack $ E.decodeUtf8 user_id))
+	writeLBS . encode $ (user :: User) 
+
+-- (Handle b v) is a Monad
+getUsers :: Handler Haskitter Haskitter [User]
+getUsers = query_ "SELECT id,email,\"name\",password_digest FROM users"
+
+getUser :: String -> Handler Haskitter Haskitter User
+getUser user_id = getUsers >>= (\users -> return (head $ (filter (equal_user_id user_id) users)))
+
+equal_user_id :: String -> User -> Bool
+equal_user_id user_id user = (show (uid user)) == user_id
